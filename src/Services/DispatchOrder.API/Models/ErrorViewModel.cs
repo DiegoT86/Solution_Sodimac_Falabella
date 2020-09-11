d2f2a30731fb48def304d90d6579fc041e31ebba
Luis Cairo <luis.cairo@globant.com>
2020-09-11 15:08:22 -0300

    Add project files.

---
+++ src/Services/DispatchOrder.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DispatchOrder.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
